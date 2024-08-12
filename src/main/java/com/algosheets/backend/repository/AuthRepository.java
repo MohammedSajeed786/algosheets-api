f3d8743af8ba55f4d1aab62c3ff67bf1d21256da
Sajeed <mdsajeedmds@gmail.com>
2024-08-12 21:27:28 +0530

    hiraki pool connections config

--- src/main/java/com/algosheets/backend/repository/AuthRepository.java
+++ src/main/java/com/algosheets/backend/repository/AuthRepository.java
@@ -2,9 +2,6 @@ package com.algosheets.backend.repository;
 
 import com.algosheets.backend.model.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
